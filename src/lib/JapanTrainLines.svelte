bb0205a659349e8aefbf54777070aa29e70127b4
Alastair Tse <alastair@liquidx.net>
2023-10-15 07:21:49 +0900

    Reimplemented train lines in Svelte component.

--- src/lib/JapanTrainLines.svelte
+++ src/lib/JapanTrainLines.svelte
@@ -1,19 +1,101 @@
 <script>
   import { onMount } from "svelte";
-  import { render } from "$lib/japan-train-lines.js";
+  import { loadTrainLines, drawTrainLine } from "$lib/japan-train-lines.js";
 
   export let railroadGeoJsonUrl = "/railroad.geojson";
-  let controlEl;
   let viewerEl;
+  let trainCompanyNames = [];
+  let selectedCompany = "東日本旅客鉄道";
+  let selectedLine = "山手線";
 
-  onMount(() => {
-    console.log("onMount", controlEl);
-    render({ railroadGeoJsonUrl, controlEl, viewerEl });
+  onMount(async () => {
+    let data = await loadTrainLines({ railroadGeoJsonUrl });
+    trainCompanyNames = data.trainCompanyNames;
+    drawTrainLine(selectedCompany, selectedLine, viewerEl);
+    registerKeyboardShortcuts({ viewerEl });
   });
+
+  const didSelectLine = (e) => {
+    let element = e.target;
+    selectedCompany = element.getAttribute("data-company");
+    selectedLine = element.getAttribute("data-line");
+    drawTrainLine(selectedCompany, selectedLine, viewerEl);
+  };
+  const didSelectCompany = (e) => {
+    let element = e.target;
+    selectedCompany = element.getAttribute("data-company");
+    selectedLine = null;
+    drawTrainLine(selectedCompany, selectedLine, viewerEl);
+  };
+
+  const registerKeyboardShortcuts = () => {
+    document.addEventListener("keydown", (e) => {
+      if (e.key === "ArrowRight") {
+        getNext();
+        drawTrainLine(selectedCompany, selectedLine, viewerEl);
+      }
+    });
+  };
+
+  const getNext = () => {
+    let nextLine = getNextLine();
+    if (selectedLine == nextLine) {
+      selectedCompany = getNextCompany();
+      selectedLine = getNextLine();
+    } else {
+      selectedLine = nextLine;
+    }
+    console.log(selectedCompany, selectedLine);
+  };
+
+  const getNextLine = () => {
+    let company = trainCompanyNames.find(
+      (company) => company.company === selectedCompany
+    );
+    let lineIndex = company.lines.indexOf(selectedLine);
+    if (lineIndex === company.lines.length - 1) {
+      return selectedLine;
+    } else {
+      return company.lines[lineIndex + 1];
+    }
+  };
+
+  const getNextCompany = () => {
+    let companyIndex = trainCompanyNames.findIndex(
+      (company) => company.company === selectedCompany
+    );
+    if (companyIndex === trainCompanyNames.length - 1) {
+      return trainCompanyNames[0].company;
+    } else {
+      return trainCompanyNames[companyIndex + 1].company;
+    }
+  };
 </script>
 
 <div id="viewer-container">
-  <div id="controls" bind:this={controlEl} />
+  <div id="controls">
+    {#each trainCompanyNames as company}
+      <div>
+        <a
+          href="#x"
+          class="train-company"
+          data-company={company.company}
+          on:click={didSelectCompany}>{company.company}</a
+        >
+      </div>
+      {#each company.lines as line}
+        <div>
+          <a
+            href="#x"
+            class="train-line"
+            data-company={company.company}
+            data-line={line}
+            on:click={didSelectLine}>{line}</a
+          >
+        </div>
+      {/each}
+    {/each}
+  </div>
   <div id="svg-viewer" bind:this={viewerEl} />
 </div>
 
--- src/lib/japan-train-lines.js
+++ src/lib/japan-train-lines.js
@@ -2,46 +2,35 @@ import { pull, concat, map } from "lodash";
 
 import { line_names } from "./train-lines.js";
 import { svg_from_segments } from "./train-line-svg.js";
-import train_line_corrections from "./train-line-corrections.json";
+import trainLineCorrections from "./train-line-corrections.json";
 
-let _train_lines = {};
+let _trainLines = {};
+let _trainCompanyNames = [];
+let _prioritizedTrainCompanies = [
+  "東京地下鉄",
+  "東急電鉄",
+  "東京モノレール",
+  "東京都",
+  "東京臨海高速鉄道",
+  "東日本旅客鉄道",
+  "西武鉄道",
+  "京成電鉄",
+  "京王電鉄",
+];
 
-let activateArrows = () => {
-  document.addEventListener("keyup", (e) => {
-    if (e.key == "ArrowRight" || e.key == "ArrowLeft") {
-      e.preventDefault();
-      let selected = document.querySelector(".line.selected");
-      if (selected) {
-        let nextSelected = null;
-        if (e.key == "ArrowRight") {
-          nextSelected = selected.nextSibling;
-        } else if (e.key == "ArrowLeft") {
-          nextSelected = selected.previousSibling;
-        }
-        let nextCompany = nextSelected.getAttribute("data-company");
-        let nextLine = nextSelected.getAttribute("data-line");
-        console.log(nextCompany, nextLine);
-        load_train_line_svg(nextCompany, nextLine);
-
-        map(document.querySelectorAll(".line"), (o) =>
-          o.classList.remove("selected")
-        );
-        nextSelected.classList.add("selected");
-      }
-    }
-    return true;
-  });
-};
-
-let load_train_line_svg = (company_name, line_name, viewerEl) => {
+export const drawTrainLine = (companyName, lineName, viewerEl, width = 640) => {
+  if (!viewerEl) {
+    console.log("Error: viewerEl is null");
+    return;
+  }
   let correction =
-    train_line_corrections[company_name] &&
-    train_line_corrections[company_name][line_name];
+    trainLineCorrections[companyName] &&
+    trainLineCorrections[companyName][lineName];
   let svg = svg_from_segments(
-    _train_lines,
-    company_name,
-    line_name,
-    640,
+    _trainLines,
+    companyName,
+    lineName,
+    width,
     correction
   );
   viewerEl.innerHTML = svg;
@@ -58,98 +47,38 @@ let load_train_line_svg = (company_name, line_name, viewerEl) => {
   }
 };
 
-let create_controls = (lines, viewerEl, controlEl) => {
-  let companies = Object.keys(lines);
+const getTrainCompanyNames = (train_lines) => {
+  let companyNames = Object.keys(train_lines);
+  // Force a few companies to the at the top.
+  companyNames = pull(companyNames, _prioritizedTrainCompanies);
+  companyNames = concat(_prioritizedTrainCompanies, companyNames);
 
-  // force a few companies to tbe at the top.
-  let manual_order_companies = [
-    "東京地下鉄",
-    "東急電鉄",
-    "東京モノレール",
-    "東京都",
-    "東京臨海高速鉄道",
-    "東日本旅客鉄道",
-    "西武鉄道",
-    "京成電鉄",
-    "京王電鉄",
-  ];
-
-  companies = pull(companies, manual_order_companies);
-  companies = concat(manual_order_companies, companies);
-
-  for (var company of companies) {
-    var d = document.createElement("a");
-    d.classList.add("train-company", "line");
-    d.setAttribute("href", `#${company}`);
-    d.setAttribute("data-company", company);
-    d.appendChild(document.createTextNode(company));
-    d.addEventListener("click", (e) => {
-      let element = e.target;
-      let company_name = element.getAttribute("data-company");
-      load_train_line_svg(company_name, null, viewerEl);
-
-      map(document.querySelectorAll(".line"), (o) =>
-        o.classList.remove("selected")
-      );
-      element.classList.add("selected");
-
-      return true;
+  let lines = [];
+  for (const company_name of companyNames) {
+    let lineNames = Object.keys(train_lines[company_name]);
+    lines.push({
+      company: company_name,
+      lines: lineNames,
     });
-    controlEl.appendChild(d);
-    if (!lines[company]) {
-      console.log("No lines for company", company);
-      continue;
-    }
-    var linesForCompany = Object.keys(lines[company]);
-
-    for (var line of linesForCompany) {
-      var l = document.createElement("a");
-      l.classList.add("train-line", "line");
-      l.setAttribute("href", `#${company}/${line}`);
-      l.setAttribute("data-line", line);
-      l.setAttribute("data-company", company);
-      l.appendChild(document.createTextNode(line));
-      l.addEventListener("click", (e) => {
-        let element = e.target;
-        let line_name = element.getAttribute("data-line");
-        let company_name = element.getAttribute("data-company");
-        load_train_line_svg(company_name, line_name, viewerEl);
-
-        map(document.querySelectorAll(".line"), (o) =>
-          o.classList.remove("selected")
-        );
-        element.classList.add("selected");
-        return true;
-      });
-      controlEl.appendChild(l);
-    }
   }
+
+  return lines;
 };
 
-export const render = ({ railroadGeoJsonUrl, controlEl, viewerEl }) => {
-  fetch(railroadGeoJsonUrl)
+export const loadTrainLines = async ({ railroadGeoJsonUrl }) => {
+  return fetch(railroadGeoJsonUrl)
     .then((response) => {
       return response.json();
     })
     .then((json) => {
       // Store to a global
-      _train_lines = json;
-      let all_lines = line_names(json);
-      create_controls(all_lines, viewerEl, controlEl);
-      let company = "東日本旅客鉄道";
-      let line = "山手線";
-      if (document.location.hash) {
-        let path = document.location.hash.replace("#", "").split("/");
-        if (path.length == 1) {
-          company = decodeURIComponent(path[0]);
-          line = null;
-        }
-        if (path.length == 2) {
-          company = decodeURIComponent(path[0]);
-          line = decodeURIComponent(path[1]);
-        }
-      }
-      load_train_line_svg(company, line, viewerEl);
-      activateArrows(_train_lines);
+      _trainLines = json;
+      let trainLineNames = line_names(json);
+      _trainCompanyNames = getTrainCompanyNames(trainLineNames);
+
+      return {
+        trainLines: _trainLines,
+        trainCompanyNames: _trainCompanyNames,
+      };
     });
 };
