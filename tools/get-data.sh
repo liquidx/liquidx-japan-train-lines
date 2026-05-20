491716762a15a397273ffd454aa8b06b7aa0ac5b
Alastair Tse <alastair@liquidx.net>
2026-05-20 20:20:33 +0900

    Update scripts to be able to fetch the data on build

---
+++ tools/get-data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Download the data for the project
+curl https://content.liquidx.net/japan-train-lines/N02-19_Station.geojson -o static/N02-19_Station.geojson
+curl https://content.liquidx.net/japan-train-lines/N02-19_RailroadSection.geojson -o static/N02-19_RailroadSection.geojson
